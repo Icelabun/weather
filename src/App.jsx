0083f61246dfa8be6f8e3046bfd6791c3dad4c25
icelabun <samuelnebiyu88@gmail.com>
2025-01-28 12:54:58 +0300

    task:update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './styles.css';
+import { Card } from 'semantic-ui-react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const CardExampleCard = ({weatherData}) => (
+  <Card>
+    <Card.Content>
+        <Card.Header className="header">City Name: {weatherData.name}</Card.Header>
+        <p>Temprature: {weatherData.main.temp}</p>
+        <p>Sunrise: {weatherData.sys.sunrise}</p>
+        <p>Sunset: {weatherData.sys.sunset}</p>
+        <p>Description: {weatherData.weather[0].description}</p>
+    </Card.Content>
+  </Card>
+)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default CardExampleCard;
